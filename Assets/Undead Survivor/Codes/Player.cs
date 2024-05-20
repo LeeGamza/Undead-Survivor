d9defb0bda67ae2c8a1eeba0dac3b91b7c7c8cd6
LeeGamza <135112596+LeeGamza@users.noreply.github.com>
2024-05-20 20:33:06 +0900

    Player_Move_Anim
    1. 캐릭터 Run, Dead, Stand 애니메이션
    2. 다른캐릭터 애니메이션 오버라이딩

--- Assets/Undead Survivor/Codes/Player.cs
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -6,10 +6,15 @@ public class Player : MonoBehaviour
 {
     public Vector2 inputVec;
     public float speed;
+    
     Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -28,5 +33,16 @@ public class Player : MonoBehaviour
     void OnMove(InputValue value)
     {
         inputVec = value.Get<Vector2>();
+    }
+
+    void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+
+        if (inputVec.x != 0) {
+            spriter.flipX = inputVec.x < 0;
+        }
+
     }
 }
