35de9531a16fb4d9967cfde26daa1ea817e6ee29
LeeGamza <135112596+LeeGamza@users.noreply.github.com>
2024-05-21 02:48:52 +0900

    5.20 마무리 (자러감)
    11

--- Assets/Undead Survivor/Codes/Spawner.cs
+++ Assets/Undead Survivor/Codes/Spawner.cs
@@ -6,9 +6,10 @@ public class Spawner : MonoBehaviour
 {
     public Transform[] spawnPoint;
 
+    int level;
     float timer;
 
-    private void Awake()
+    void Awake()
     {
         spawnPoint = GetComponentsInChildren<Transform>();
     }
@@ -16,17 +17,21 @@ public class Spawner : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
+        level = Mathf.FloorToInt(GameManager.instance.gameTime / 10f);
 
-        if (timer > 0.2f)
+        if (timer > (level == 0 ? 0.5f : 0.2f))
         {
             timer = 0;
             Spawn();
         }
+       
     }
 
     void Spawn()
     {
-        GameObject enemy = GameManager.instance.pool.Get(Random.Range(0, 2));
+        GameObject enemy = GameManager.instance.pool.Get(level);
         enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
     }
+
+    
 }
