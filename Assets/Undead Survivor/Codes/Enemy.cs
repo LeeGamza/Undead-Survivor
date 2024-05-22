ae8ba409c27e5b6f5fe4ea3b79321c3ddfd9c829
LeeGamza <135112596+LeeGamza@users.noreply.github.com>
2024-05-22 16:20:10 +0900

    ing(2)
    ing

--- Assets/Undead Survivor/Codes/Enemy.cs
+++ Assets/Undead Survivor/Codes/Enemy.cs
@@ -13,19 +13,22 @@ public class Enemy : MonoBehaviour
     bool isLive;
 
     Rigidbody2D rigid;
+    Collider2D coll;
     Animator anim;
     SpriteRenderer spriter;
-
+    WaitForFixedUpdate wait;
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
         anim = GetComponent<Animator>();
         spriter = GetComponent<SpriteRenderer>();
+        wait = new WaitForFixedUpdate();
     }
 
     void FixedUpdate()
     {
-        if (!isLive)
+        if (!isLive || anim.GetCurrentAnimatorStateInfo(0).IsName("Hit")) 
             return;
 
         Vector2 dirVec = target.position - rigid.position;
@@ -56,26 +59,41 @@ public class Enemy : MonoBehaviour
         health = data.health;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if (!collision.CompareTag("Bullet"))
                 return;
 
         health -= collision.GetComponent<Bullet>().damage;
+        StartCoroutine(KnockBack());
 
         if (health > 0)
         {
+            anim.SetTrigger("Hit");
 
         }
 
         else
         {
+            isLive = false;
+            coll.enabled = false;
+            rigid.simulated = false;
+            spriter.sortingOrder = 1;
+            anim.SetBool("Dead", true);
             Dead();
         }
 
-        void Dead()
-        {
-            gameObject.SetActive(false);
-        }
+ 
+    }
+    IEnumerator KnockBack()
+    {
+        yield return wait;
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 dirVec = transform.position - playerPos;
+        rigid.AddForce(dirVec.normalized * 3, ForceMode2D.Impulse);
+    }
+    void Dead()
+    {
+        gameObject.SetActive(false);
     }
 }
